d0b9301c7d40eecdb8c4095f1401ba5fd14d1c39
Quy Leu Duc <quy.leu@kyanon.digital>
2023-12-15 15:37:22 +0700

    modify watch

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 POCKETWATCH All Rights Reserved</p>
+      <p>2023 QUY WATCH All Rights Reserved</p>
       <p className='icons'>
         <AiFillInstagram/>
         <AiOutlineTwitter/>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/ ">POCKETWATCH</Link>
+        <Link href="/ ">QUY WATCH</Link>
       </p>
 
       <button type='button' className='cart-icon' onClick={()=> setShowcart(true)}>
